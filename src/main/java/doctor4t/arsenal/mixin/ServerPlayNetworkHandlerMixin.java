910988e48bb715a2c66ead1a10ba5aedf27e7b0a
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2023-03-08 21:40:26 +0000

    no more dupe bug lmao

--- src/main/java/doctor4t/arsenal/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/doctor4t/arsenal/mixin/ServerPlayNetworkHandlerMixin.java
@@ -1,9 +1,14 @@
 package doctor4t.arsenal.mixin;
 
 import doctor4t.arsenal.common.util.WeaponSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.NetworkThreadUtils;
+import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,6 +23,15 @@ public class ServerPlayNetworkHandlerMixin {
 	private void arsenal$updateSlot(UpdateSelectedSlotC2SPacket packet, CallbackInfo ci) {
 		if (packet instanceof WeaponSlot selectPacket && this.player.getInventory() instanceof WeaponSlot selection) {
 			selection.arsenal$setWeaponSlot(selectPacket.arsenal$getWeaponSlot());
+		}
+	}
+
+	@Inject(method = "onPlayerAction", at = @At(value = "HEAD"))
+	private void arsenal$swapHands(PlayerActionC2SPacket packet, CallbackInfo ci) {
+		if (packet.getAction() == PlayerActionC2SPacket.Action.SWAP_ITEM_WITH_OFFHAND) {
+			if (this.player instanceof WeaponSlot weaponSlot) {
+				weaponSlot.arsenal$setWeaponSlot(false);
+			}
 		}
 	}
 }
