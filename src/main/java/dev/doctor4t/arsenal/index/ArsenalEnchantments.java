e8a8540945f5d1fbaf84b20103222cec5882cafc
doctor4t <doctor4t@gmail.com>
2024-12-13 20:45:42 +0100

    Bump version

--- src/main/java/dev/doctor4t/arsenal/index/ArsenalEnchantments.java
+++ src/main/java/dev/doctor4t/arsenal/index/ArsenalEnchantments.java
@@ -18,7 +18,7 @@ public interface ArsenalEnchantments {
 
     Enchantment SPEWING = createEnchantment("spewing", new SpewingEnchantment(Enchantment.Rarity.RARE, EquipmentSlot.MAINHAND));
     Enchantment REELING = createEnchantment("reeling", new ReelingEnchantment(Enchantment.Rarity.RARE, EquipmentSlot.MAINHAND));
-    Enchantment HEFT = createEnchantment("heft", new HeftEnchantment(Enchantment.Rarity.RARE, EquipmentSlot.MAINHAND));
+//    Enchantment HEFT = createEnchantment("heft", new HeftEnchantment(Enchantment.Rarity.RARE, EquipmentSlot.MAINHAND));
 
     private static Enchantment createEnchantment(String name, Enchantment enchantment) {
         ENCHANTMENTS.put(enchantment, new Identifier(Arsenal.MOD_ID, name));
